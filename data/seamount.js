2735b605055f43dc096fca738064c31a6205e3ed
Julia <>
2022-07-06 17:04:38 +0200

    first commit :octocat:

---
+++ data/seamount.js
@@ -0,0 +1,68 @@
+const seamounts = [
+    {
+       "Name":"New England Seamount Chain",
+       "Type":"point",
+       "Latitude":37.4,
+       "Longitude":-60,
+       "Period of formation":"103 to 100 million years ago",
+       "Height":"Most of its peaks rise over 4,000m from the seabed",
+       "Length":"nearly 1,200km",
+       "Description":"A chain of over 20 extinct underwater volcanoes, it is the longest seamount chain in the North Atlantic Ocean.",
+       "Biodiversity":"A rich diversity of corals, sponges and crustaceans like krill. A species of cutthroat eel is also found in the area."
+    },
+    {
+       "Name":"Mauna Kea",
+       "Type":"point",
+       "Latitude":19.820667,
+       "Longitude":-155.468056,
+       "Period of formation":"at least 1 million years ago",
+       "Height":"9,966m (5,761m of it underwater and 4,205m above sea level)",
+       "Length":"not known",
+       "Description":"One of the six volcanoes that form the islands of Hawaii. If measured from its underwater base, it beats Everest’s peak by 1,116 meters. Currently inactive, it last erupted 4,600 years ago.",
+       "Biodiversity":"A variety of marine animals, from humpback whales and Hawaiian pink snappers to sea cucumber and Abyssal cusk eels, can be found in the area."
+    },
+    {
+       "Name":"Hawaiian–Emperor Seamount Chain",
+       "Type":"point",
+       "Latitude":19.820556,
+       "Longitude":-155.468056,
+       "Period of formation":"81 million years ago",
+       "Height":"Its highest peak is Mauna Kea, which rises 9,966m from the seafloor",
+       "Length":"6,200km",
+       "Description":"This vast undersea mountain range is formed by the Hawaiian volcanic islands and the Emperor Seamounts. Not only seamounts and islands, but also atolls, shallows and banks can be found in this topographically diverse region.",
+       "Biodiversity":"Tunas, whales, seabirds, cold-water coals, and sponges are found along the seamount chain. They make forests of fans, trees and whips that host a diversity of fish species and invertebrates."
+    },
+    {
+       "Name":"Tasmantid Seamount Chain",
+       "Type":"point",
+       "Latitude":-31,
+       "Longitude":156,
+       "Period of formation":"40 to 6 million years ago",
+       "Height":"Some peaks rise more than 4,000m from the seabed",
+       "Length":"2,000km",
+       "Description":"A chain of over 16 extinct volcanic peaks located east of the Australian mainland. They provide scientists with an important record of how the Australian Plate has changed velocity over time.",
+       "Biodiversity":"Some of the seamounts support an abundance of dogfish including Harrisson's dogfish and seamount spurdogs."
+    },
+    {
+       "Name":"Cascade Seamount",
+       "Type":"point",
+       "Latitude":-43.9158617,
+       "Longitude":150.3834619,
+       "Period of formation":"56 to 33.9 million years ago",
+       "Height":"The highest point reaches up to 598m below sea level",
+       "Length":"not known",
+       "Description":"This underwater mountain was once a volcanic island that reached 400m above sea level. It now sits atop the East Tasman Plateau and was discovered in 1944.",
+       "Biodiversity":"Several kinds of whales, albatrosses and petrels live in and above the water around it. The deepwater seastar Novodinia australis also inhabits the seamount."
+    },
+    {
+       "Name":"Axial Seamount",
+       "Type":"point",
+       "Latitude":46.0603859,
+       "Longitude":-129.9998283,
+       "Period of formation":"31,000 years ago",
+       "Height":"700m above the mean level of the central Juan de Fuca Ridge",
+       "Length":"not known",
+       "Description":"An active deep-sea volcano located on the Juan de Fuca Ridge, around 470km off the coast of Oregon, US. Its eruption in 2015 makes it the most active volcano in the northeastern Pacific basin.",
+       "Biodiversity":"Several species of bony fish are found living in the nearby water. While spider crabs and squat lobsters are common inhabitants of the mount itself. There are also seastars, brittle stars, urchins and sea cucumbers."
+    }
+ ]
---
+++ data/trenches.js
@@ -0,0 +1,112 @@
+const trenches = [
+    {
+       "Name":"Mariana Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7378",
+       "Latitude":16.6911279,
+       "Longitude":147.8033746,
+       "Length":"2,550km",
+       "Depth":"Deepest point: Challenger Deep, 10.91km",
+       "Description":"Considered to be the deepest part of the Earth’s surface, the deep chasms of the Mariana Trench were formed by the collision of converging plates.",
+       "Biodiversity":"Expeditions have observed the presence of large creatures such as a flatfish, large shrimp, huge crustaceans and even an unidentified type of snailfish."
+    },
+    {
+       "Name":"Tonga Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7568",
+       "Latitude":-20.38268084,
+       "Longitude":-173.2297796,
+       "Length":"2,500km",
+       "Depth":"Deepest point: Horizon Deep, 10.82km",
+       "Description":"Located in the south-west Pacific Ocean and at the Kermadec Tonga Subduction Zone’s northern end. It is considered the second deepest place on Earth after Challenger Deep in the Mariana Trench. Its deepest point is known as the Horizon Deep.",
+       "Biodiversity":"The sediments of the Horizon Deep houses a community of roundworms."
+    },
+    {
+       "Name":"Philippine Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=6952",
+       "Latitude":10.11973855,
+       "Longitude":126.6790779,
+       "Length":"1,320km",
+       "Depth":"Deepest point:  Galathea Depth, 10.54km",
+       "Description":"This submarine trench was formed by a collision between the Eurasian Plate and the smaller Philippine Plate. It is younger than 8 or 9 million years old. The Galathea Depth in the trench is the third deepest known point in the world.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Kuril–Kamchatka Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7926",
+       "Latitude":46.78141618,
+       "Longitude":154.6894373,
+       "Length":"2,100km",
+       "Depth":"10.5km",
+       "Description":"Lying close to Kuril Island and off the coast of Kamchatka, this trench is responsible for a number of volcanic activities in the region. It was formed due to the subduction zone developed in the late Cretaceous, which created Kuril Island and the Kamchatka volcanic arcs.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Kermadec Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7571",
+       "Latitude":-31.02880762,
+       "Longitude":-176.9371064,
+       "Length":"1,000km",
+       "Depth":"10.04km",
+       "Description":"Located between the Louisville Seamount Chain and the Hikurangi Plateau. It was formed by the subduction of the Pacific Plate under the Indo-Australian Plateau. Along with the Tonga Trench to the north, the Kermadec Trench creates the 2,000km-long Kermadec–Tonga subduction system.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Izu–Ogasawara Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=6043",
+       "Latitude":32,
+       "Longitude":142.166667,
+       "Length":"not known",
+       "Depth":"9.78km",
+       "Description":"Stretches from Japan to the northern section of Mariana Trench and is also an extension of the Japan Trench. Apart from the Izu–Ogasawara Trench, the western Pacific Ocean also houses the Izu Trench and the Bonin Trench.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Japan Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7382",
+       "Latitude":38.15233163,
+       "Longitude":144.0349849,
+       "Length":"not known",
+       "Depth":"9km",
+       "Description":"Part of the Pacific Ring of Fire in the northern Pacific Ocean, it stretches from the Kuril Islands to the Bonin Islands and is an extension of the Kuril–Kamchatka Trench and the Izu–Ogasawara Trench to the north and south respectively. It was formed due to the subduction of the oceanic Pacific Plate beneath the continental Okhotsk Plate. It is tsunamis and earthquakes that lead to the movement on the subduction zone with the Japan Trench.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Puerto Rico Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=4587",
+       "Latitude":19.91893865,
+       "Longitude":-65.20084991,
+       "Length":"800km",
+       "Depth":"Deepest point: Milwaukee Deep, 8.64km",
+       "Description":"Located between the Caribbean Sea and the Atlantic Ocean, it marks the deepest point in this region, with Milwaukee Deep being its lowest point. This trench has been responsible for many tragic tsunamis and earthquake activities in this region.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"South Sandwich Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=6753",
+       "Latitude":-56.41733166,
+       "Longitude":-25.2613526,
+       "Length":"956km",
+       "Depth":"Deepest point: Meteor Deep, 8.42km",
+       "Description":"Located 100km to the east of the South Sandwich Islands in the southern Atlantic Ocean, it was formed by the subduction of the South American Plate’s southernmost portion beneath the small South Sandwich Plate. This trench is also associated with an active volcanic arc.",
+       "Biodiversity":""
+    },
+    {
+       "Name":"Peru–Chile “Atacama” Trench",
+       "Type":"point",
+       "Location":"https://marineregions.org/gazetteer.php?p=details&id=7587",
+       "Latitude":-25.78213487,
+       "Longitude":-71.67574794,
+       "Length":"5,900km",
+       "Depth":"Deepest point: Richards Deep, 8.06km",
+       "Description":"Also known as Atacama Trench, it is located around 160km off the coast of Peru and Chile in the eastern Pacific Ocean. It has a maximum depth of 8.06 km below sea level. It was formed as a result of a convergent boundary, between the subducting Nazca and the South American plates.",
+       "Biodiversity":""
+    }
+ ]
