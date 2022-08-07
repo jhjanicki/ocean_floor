30688c133bd2323ba2fd369a63169d5212008f85
Julia <>
2022-08-07 19:34:52 +0200

    updated coordinates

---
+++ data2/ridges_points.js
@@ -0,0 +1,34 @@
+const ridges_points = [{
+	"Name": "Arctic Mid-Ocean Ridge",
+	"Type": "point",
+	"Location references for Julia": "https://marineregions.org/gazetteer.php?p=details&id=7819",
+	"latitude": 86.55921619,
+	"longitude": 77.85966444,
+	"Period_of_formation": "1.1 million years ago",
+	"Height": "N/A",
+	"Depth": "N/A",
+	"Length": "1,800km",
+	"Description": "Also known as the Gakkel Ridge. It forms the boundary between the North American Plate and the Eurasian Plate. It was discovered by a Soviet Union expedition to the central Arctic in 1948. Its largest volcano erupted approximately 1.1 million years ago."
+}, {
+	"Name": "Explorer Ridge",
+	"Type": "point",
+	"Location references for Julia": "http://marineregions.org/mrgid/19275",
+	"latitude": 49.45,
+	"longitude": -130.58333,
+	"Period_of_formation": "7 to 5 million years ago",
+	"Height": "N/A",
+	"Depth": "N/A",
+	"Length": "N/A",
+	"Description": "Situated about 241km west of Vancouver Island, it is seismically active causing regular earthquakes in the area."
+}, {
+	"Name": "Gorda Ridge",
+	"Type": "point",
+	"Location references for Julia": "http://marineregions.org/mrgid/6404",
+	"latitude": 41.75,
+	"longitude": -127,
+	"Period_of_formation": "30 million years ago",
+	"Height": "N/A",
+	"Depth": "N/A",
+	"Length": "300km",
+	"Description": "It originated from the subduction of the Farallon Plate beneath the North American Plate, which created new microplates and ridges, including the Juan de Fuca. Further segmentation of the Juan de Fuca Plate led to the formation of the Gorda Plate and Gorda Ridge."
+}];
